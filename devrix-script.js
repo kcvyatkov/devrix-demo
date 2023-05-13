9f4d530249bb9ae2203f3bb49fea4fb031c2e564
kcvyatkov <kaloyancvyatkov@gmail.com>
2023-05-13 11:04:17 +0300

    first commit

---
+++ devrix-script.js
@@ -0,0 +1,24 @@
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Sofia,bg&appid=b756f2f26b03879b87e8a2edfa36af69')
+    .then(response => response.json())
+    .then(data => {
+        const temperature = Math.round(data.main.temp - 273.15); // convert temperature from Kelvin to Celsius
+        document.getElementById('weather').innerHTML = ` ${temperature}&deg;, Sofia`;
+    })
+    .catch(error => console.error(error));
+
+
+
+
+        document.addEventListener('DOMContentLoaded', function() {
+            // Get the hamburger and header-nav elements
+            const hamburger = document.querySelector('.hamburger');
+            const headerNav = document.querySelector('.header-nav');
+        
+            // Add an event listener to the hamburger element
+            hamburger.addEventListener('click', function() {
+            // Toggle the class to show/hide the header-nav
+            headerNav.classList.toggle('show');
+            // Toggle the close class on the hamburger icon
+  hamburger.classList.toggle('close');
+            });
+        });
